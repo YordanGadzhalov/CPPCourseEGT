2b0a14ea6c8b3a6e5cdd5e341109c2f52455c678
Yordan Gadzhalov <100886252+YordanGadzhalov@users.noreply.github.com>
2024-01-31 16:06:39 +0200

    EvenOrOddNumbers

---
+++ EvenOrOddNumbers_homework/EvenOrOdd.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cout << "How many numbers do you want to check ?" << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int number;
+        cin >> number;
+        if (number % 2 == 0) {
+            cout << "The number is even!" << endl;
+        }
+        else {
+            cout << "The number is odd!" << endl;
+        }
+    }
+}
