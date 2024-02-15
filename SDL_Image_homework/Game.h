8656c53cce65cddf8b5844d640e31e9de5333b2e
Yordan Gadzhalov <100886252+YordanGadzhalov@users.noreply.github.com>
2024-02-15 11:19:27 +0200

    Upload homework

---
+++ SDL_Image_homework/Game.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "TextureManager.h"
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+
+	void render();
+	void update();
+	void handleEvent();
+	void clean();
+
+	bool isRunning() const;
+
+private:
+	SDL_Window* window = NULL;
+	SDL_Renderer* renderer = NULL;
+	bool running;
+	int mouseDownX, mouseDownY;
+	int mouseDown, mouseUp;
+
+	SDL_RendererFlip frameFlip = SDL_FLIP_NONE;
+	int currentFrame;
+	int currentRow;
+}; 
